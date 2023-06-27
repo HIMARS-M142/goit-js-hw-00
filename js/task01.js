af5e557ecc2b81fec19c9f0ea8f957221d9fb2bb
HIMARS-M142 <maksympona@gmail.com>
2023-06-27 18:14:09 +0200

    upd

---
+++ js/task01.js
@@ -0,0 +1,5 @@
+const allItemEl = document.querySelectorAll(".item");
+const numberLengthEl = `Number of categories: ${allItemEl.length}`;
+console.log(numberLengthEl);
+
+const allTitleEl = document.querySelectorAll("h2");
